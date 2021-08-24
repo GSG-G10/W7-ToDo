b92392978a0f14db1bff7d6c05481dda78b872c5
muath-khatib <mo3az.khatib@hotmail.com>
2021-08-24 23:56:36 +0300

    some edites for deploy Relates #10

--- server/controllers/Addtask.js
+++ server/controllers/Addtask.js
@@ -1,19 +1,8 @@
 const { addTaskQuery } = require('../database/queries');
 
 module.exports = (req, res) => {
-  console.log('in function')
   const { taskName, dueTime } = req.body;
-  console.log(taskName, dueTime)
   addTaskQuery(taskName, dueTime)
-    // eslint-disable-next-line no-unused-vars
     .then((result) => res.redirect('/home'))
-    // eslint-disable-next-line no-unused-vars
-
-    .catch((err) => console.log(err));
-
-
+    .catch((err) => res.send(err.message));
 };
-
-
-
-
--- server/controllers/get_tasks.js
+++ server/controllers/get_tasks.js
@@ -1,7 +1,7 @@
 const { getTasksQuery } = require('../database/queries');
 
 module.exports = (req, res, next) => {
-  getTasksQuery(15)
+  getTasksQuery(1)
     .then((results) => res.json(results.rows))
     .catch(next);
 };
