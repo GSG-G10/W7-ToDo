2547502598ac2b90345cfec53fd96dfad244e1d4
muhammadharoun <hro19502001@gmail.com>
2021-08-24 22:58:31 +0300

    fix add bug

--- server/controllers/Addtask.js
+++ server/controllers/Addtask.js
@@ -1,12 +1,17 @@
 const { addTaskQuery } = require('../database/queries');
 
 module.exports = (req, res) => {
+  console.log('in function')
   const { taskName, dueTime } = req.body;
+  console.log(taskName, dueTime)
   addTaskQuery(taskName, dueTime)
     // eslint-disable-next-line no-unused-vars
     .then((result) => res.redirect('/home'))
     // eslint-disable-next-line no-unused-vars
-    .catch((err) => res.redirect('/error'));
+
+    .catch((err) => console.log(err));
+
+
 };
 
 
--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -1,5 +1,8 @@
 const getTasksQuery = require('./get_task_query');
+const addTaskQuery = require('./add_tasks_query');
+
 
 module.exports = {
   getTasksQuery,
+  addTaskQuery,
 };
