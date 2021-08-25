cd0df9f45f1609239daa4c0923d0a10928cecbbd
muhammadharoun <hro19502001@gmail.com>
2021-08-25 10:43:16 +0300

    create compare passwords function

--- server/middlewares/hash_password.js
+++ server/middlewares/hash_password.js
@@ -5,6 +5,11 @@ const hashPassword = (password, callback) => {
   bcrypt.hash(password, salt, callback);
 };
 
+const comparePasswords = (password, hashedPassword, callback) => {
+  bcrypt.compare(password, hashedPassword, callback);
+};
+
 module.exports = {
+  comparePasswords,
   hashPassword,
 };
--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,9 +1,12 @@
 const checkIsUser = require('./check_is_user');
 const loginToken = require('./login_cookie');
 const getAuth = require('./get_Auth');
+const { comparePasswords, hashPassword } = require('./hash_password');
 
 module.exports = {
   checkIsUser,
   loginToken,
   getAuth,
+  hashPassword,
+  comparePasswords,
 };
