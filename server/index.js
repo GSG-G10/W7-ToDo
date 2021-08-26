a19beeee66e00cc12599deb44eaf5cd2bac27dc5
umar <umars.salah@gmail.com>
2021-08-26 12:06:37 +0300

    Initialize test for routes and database Relates #44

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-app.listen(app.get('PORT'), () => {
+app.listen(app.get('port'), () => {
   // eslint-disable-next-line no-console
-  console.log(`Server is listening at http://localhost:${app.get('PORT')}`);
+  console.log(`Server is listening at http://localhost:${app.get('port')}`);
 });
